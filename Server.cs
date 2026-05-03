6d06a152297dee906b67494ed345ce373726b77e
Aleksa Milivojevic <aleksa.milivojevic@elfak.rs>
2026-05-03 20:08:24 +0200

    add graceful sh and listener thread

--- Server.cs
+++ Server.cs
@@ -1,5 +1,9 @@
 using System.Net;
+using System.Threading;
+using System.Runtime.InteropServices;
 using FileUtil;
+
+#define maxThreads 100
 
 namespace ProjectServer
 {
@@ -15,21 +19,41 @@ namespace ProjectServer
 
         private bool ShutDownRequested = false;
 
+        private SemaphoreSlim _sem;
+
+        private List<HttpListenerContext> _requests;
+
         public Server() {
             _listener = new HttpListener();
             _listener.Prefixes.Add(hostUrl);
             //_worker = new FileWorker();
             _threads = new List<Thread>();
+            _sem = new SemaphoreSlim(0, maxThreads);
+            _requests = new List<HttpListenerContext>();
         }
 
         // dodaj ogranicenje za broj istovremenih thread-ova
         public void Start() {
-            _listener.Start();
+
+            Thread shutDownThread = new Thread(() => {
+                GracefulShutdown();
+            }).Start();
+
+            Thread listenerThread = new Thread(() => {
+                _listener.Start();
+                
+                while (!ShutDownRequested) {
+                    HttpListenerContext context = _listener.GetContext();
+                    _requests.add(context);
+                }
+            }).Start();
 
             while(!ShutDownRequested) {
-                HttpListenerContext context = _listener.GetContext();
-                HttpListenerRequest request = context.Request;
-                HttpListenerResponse response = context.Response;
+                _sem.Wait();
+
+                HttpListenerRequest request = _requests[0].Request;
+                HttpListenerResponse response = _requests[0].Response;
+                _requests.RemoveAt(0);
 
                 if (request.HttpMethod != "GET") {
                     Console.WriteLine("[Server] Discarded a non-get http request...");
@@ -46,9 +70,7 @@ namespace ProjectServer
                 ShutDownRequested = true;
             }
 
-            foreach (var thread in _threads) {
-                thread.Join();
-            }
+            shutDownThread.Join();
         }
 
         private void RequestHandle(HttpListenerRequest request, HttpListenerResponse response) {
@@ -67,6 +89,35 @@ namespace ProjectServer
             System.IO.Stream output = response.OutputStream;
             output.Write(buffer, 0, buffer.Length);
             output.Close();
+
+            _sem.Release();
+        }
+
+        private GracefulShutdown() {
+            var waitForExit = new ManualResetEventSlim(false);
+            
+            PosixSignalRegistration.Create(PosixSignal.SIGINT => {
+                Console.WriteLine("[Server] SIGINT called");
+                Console.WriteLine("[Server] Shutting down...");
+
+                ShutDownRequested = true;
+
+                _listenerThread.Join();
+
+                foreach(var request in _requests) {
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes("<HTML><BODY>Server shut down</BODY></HTML>");
+                    System.IO.Stream output = request.response.OutputStream;
+                    ouput.Write(buffer, 0, buffer.Length);
+                    output.Close();
+                }
+       
+                foreach (var thread in _threads) {
+                    thread.Join();
+                }
+
+            });
+
+            waitForExit.Wait();
         }
     }
 }
